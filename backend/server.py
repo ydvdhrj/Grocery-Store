6ef8d0f6eead5656795c19e4fba2a657cdb8dc8b
ydvdhrj <ydv.dhrj123@gmail.com>
2024-10-14 22:50:58 +0530

    second commit

---
+++ backend/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import products_dao
+from sql_connection import get_sql_connection
+
+app = Flask(__name__)
+
+connection = get_sql_connection()
+
+@app.route('/getProducts', methods=['GET'])
+def get_products():
+    products = products_dao.get_all_products(connection)
+    response = jsonify(products)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/deleteProducts', methods=['POST'])
+def get_products():
+    products = products_dao.get_all_products(connection, request.form['product_id'])
+    response = jsonify({
+        'product_id': return_id
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+if __name__ == "__main__":
+    print("starting python flask server for grocery store management")
+    app.run(port=5000)
